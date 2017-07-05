47071ae6aff84734575b83f68b586c206b551304
Jason Smith <jason.smith.webmail@gmail.com>
2017-07-05 00:32:46 -0500

    added date stuff

--- test.sh
+++ test.sh
@@ -12,8 +12,23 @@ then
 fi
 
 tickerName=$1
-curl -s "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=$tickerName&qopts.columns=date,close,high,low&api_key=UsYsv7dKGxHHQ5oURP4B" | jq -c .datatable.data | jq -c . | perl -pe 's/\["([0-9]{4}-[0-9]{2}-[0-9]{2})",(.*?),(.*?),(.*?)\]/{"date": "\1", "close": \2, "high": \3, "low": \4}/g' | jq . > data/${tickerName}stock.json
+cat data/historical.txt | grep -i ${tickerName} > data/historytemp.txt
+while read p; do
+  echo $p
+  tickerNameVar=`echo $p | awk '{print $1}'`
+  labelType=`echo $p | awk '{print $2}'`
+  labelTypeDate=`echo $p | awk -F" " '{print $3}'`
+  # need to calc start date to be 4 weeks prior and end date to be one week after;
+    #date -d"2014-09-11 -4 week"
+    #date -d"2014-09-11 +1 week"
+  # need to add start and end for quandl query based on $labelTypeDate
+  # need to print template for each iteration in loop to build total template.
+done <data/historytemp.txt
+
+
+#curl -s "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=$tickerName&qopts.columns=date,close,high,low&api_key=UsYsv7dKGxHHQ5oURP4B" | jq -c .datatable.data | jq -c . | perl -pe 's/\["([0-9]{4}-[0-9]{2}-[0-9]{2})",(.*?),(.*?),(.*?)\]/{"date": "\1", "close": \2, "high": \3, "low": \4}/g' | jq . > data/${tickerName}stock.json
 
 #cat template/testtemplate.txt|sed "s/tickerName/$tickerName/g"
 
-cat data/historical.txt | grep -i ${1}
+#sample quandl query with start and end dates;
+#curl "https://www.quandl.com/api/v3/datasets/WIKI/FB.json?column_index=4&start_date=2014-01-01&end_date=2014-12-31&collapse=monthly&transform=rdiff&api_key=YOURAPIKEY"
