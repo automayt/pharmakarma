3fd4e0bc80e16039d30dc60a0cd11d0e2f142371
root <root@LAPTOP-S7UQ808M.localdomain>
2018-06-12 22:12:35 -0500

    updated with analysis, history, and more

--- pharmakarma.sh
+++ pharmakarma.sh
@@ -19,6 +19,22 @@ fi
 
 tickerName=$1
 cat data/historical.txt | grep -i ${tickerName} > data/historytemp.txt
+#Analyze
+marketCap=$(curl -s "https://www.quandl.com/api/v3/datatables/SHARADAR/SF1.csv?ticker=VRX&qopts.columns=marketcap&api_key=UsYsv7dKGxHHQ5oURP4B" | tail -1)
+rm analysistemp.test
+if grep -iq crl "data/historytemp.txt"; then
+  grep "CRL" template/analysis.txt >> analysistemp.test
+  else
+  grep "No CRL" template/analysis.txt >> analysistemp.test
+fi
+if [ "$marketCap" -ge 2000000000 -a "$marketCap" -le 10000000000 ]; then 
+ grep "midsize" template/analysis.txt >> analysistemp.test
+elif [ "$marketCap" -gt 10000000000 ]; then
+ grep "largesize" template/analysis.txt >> analysistemp.test
+elif [ "$marketCap" -lt 2000000000 ]; then
+ grep "largesize" template/analysis.txt >> analysistemp.test
+fi
+
 while read p; do
   tickerNameVar=`echo $p | awk '{print $1}'`
   labelType=`echo $p | awk '{print $2}'|tr -dc '[:alnum:]\n\r'`
@@ -27,16 +43,34 @@ while read p; do
   newDate=`date -d"${labelTypeDate} +2 week" +%F`
   titleHere=`echo ${tickerNameVar} ${labelType} on ${labelTypeDate}`
 
-#working, need to uncomment
+
+#Create Info
+cat data/historical.txt | grep -i vrx | sed 's/^/<div>/g'| sed 's/$/<\/div>/g' > infotemp.test
+
+#Create Links
+cat template/linktemplace.txt | sed "s/tickerName/${tickerName}/g" > linktemp.test
+
+#Create Data
 curl -s "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=$tickerName&date.gt=$oldDate&date.lt=$newDate&qopts.columns=date,close,high,low&api_key=UsYsv7dKGxHHQ5oURP4B" | jq -c .datatable.data | jq -c . | perl -pe 's/\["([0-9]{4}-[0-9]{2}-[0-9]{2})",(.*?),(.*?),(.*?)\]/{"date": "\1", "close": \2, "high": \3, "low": \4}/g' | jq . > data/${tickerNameVar}_${labelType}_${labelTypeDate}.json
-allData+=`cat template/testtemplate.txt | sed "s/tickerName_labelType_labelTypeDate/${tickerNameVar}_${labelType}_${labelTypeDate}/g" | sed "s/labelTypeDate/${labelTypeDate}/g"| sed "s/labelType/${labelType}/g" | sed "s/titleHere/${titleHere}/g"`
+allData+=`cat template/datatemplate.txt | sed "s/tickerName_labelType_labelTypeDate/${tickerNameVar}_${labelType}_${labelTypeDate}/g" | sed "s/labelTypeDate/${labelTypeDate}/g"| sed "s/labelType/${labelType}/g" | sed "s/titleHere/${titleHere}/g"`
 done <data/historytemp.txt
-echo $allData > temp.test
+echo $allData > datatemp.test
 
-#cat template/conf.html | sed -e "s/replaceDataHere/${allData}/g"
-
-sed '/replaceDataHere/{
+cat template/conf.html | sed '/replaceInfoHere/{
+    s/replaceInfoHere//g
+    r infotemp.test
+}' | sed '/replaceLinksHere/{
+    s/replaceLinksHere//g
+    r linktemp.test
+}' | sed '/replaceDataHere/{
     s/replaceDataHere//g
-    r temp.test
-}' template/conf.html
-rm temp.test
+    r datatemp.test
+}' | sed '/replaceAnalysisHere/{
+    s/replaceAnalysisHere//g
+    r analysistemp.test
+}'
+
+
+
+rm *.test
+
