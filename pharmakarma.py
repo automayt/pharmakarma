cd85d79fdde611fc6a7bbfc6b86656184aa67f72
Jason Smith <jason.smith.webmail@gmail.com>
2018-11-19 17:23:59 -0600

    Added update warning tests

--- pharmakarma.py
+++ pharmakarma.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, make_response, redirect, url_for
+from flask import Flask, render_template, make_response
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 import pandas as pd
@@ -10,9 +10,6 @@ from datetime import datetime, timedelta
 import random
 import io
 import time
-
-
-
 
 app = Flask(__name__)
 # Configure a secret SECRET_KEY
@@ -31,34 +28,37 @@ class InfoForm(FlaskForm):
 
 def index():
     stockpick = False
-    updatehistory = False
     # Create instance of the form.
     form = InfoForm()
     # If the form is valid on submission (we'll talk about validation next)
     if form.validate_on_submit():
+
         # Grab the data from the stockpick on the form.
         stockpick = form.stockpick.data.upper()
         # Reset the form's stockpick data to be False
-        # form.stockpick.data = ''
-        time.sleep(2)
+        form.stockpick.data = ''
+
 
         one_hour_ago = datetime.now() - timedelta(hours=1)
         if os.path.exists("history.csv"):
             filetime = datetime.fromtimestamp(os.path.getctime("history.csv"))
-            if filetime < one_hour_ago:
+            updateneeded="test"
+            time.sleep(2)
+
+            if filetime > one_hour_ago:
+
                 histdata = pd.read_html("https://www.biopharmcatalyst.com/calendars/historical-catalyst-calendar")
                 histdata[0].to_csv('history.csv',index=False)
         else:
-            updatehistory = "test"
             histdata = pd.read_html("https://www.biopharmcatalyst.com/calendars/historical-catalyst-calendar")
             histdata[0].to_csv('history.csv',index=False)
         df = pd.read_csv('history.csv').set_index('Ticker')
         df[["Date","Catalyst"]] = df.Catalyst.str.extract('(?P<Date>[0-9]{2}\/[0-9]{2}\/[0-9]{4})(?P<Catalyst>.*)', expand=True)
         data = df.loc[stockpick]
         pd.set_option('display.max_colwidth', -1)
-        return render_template('view.html',form=form, updatehistory=updatehistory, tables=[data.to_html(classes="stockframe")])
+        return render_template('view.html',form=form, updateneeded=updateneeded, tables=[data.to_html(classes="stockframe")])
     else:
-        return render_template('home.html',form=form, updatehistory=updatehistory, stockpick=stockpick)
+        return render_template('home.html',form=form, stockpick=stockpick)
 
 @app.route('/plot.png')
 def plot():
